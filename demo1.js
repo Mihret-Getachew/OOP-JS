aab76d6430a5866aa762f9179d7fd07b9adb835a
Mihret_GetachewE <mgetachew@miu.edu>
2023-10-30 16:18:52 -0500

    Add Files

---
+++ demo1.js
@@ -0,0 +1,31 @@
+// let X = 5;
+// let Y = 5;
+// let SUM = X + Y;
+// console.log(5 + 5);
+
+// const prompt = require("prompt-sync")({ sigint: true });
+// // let name = prompt("What is your name?: ")
+// // console.log("Hi ", name);
+
+// // import promptSync from "prompt-sync";
+// // const prompt = promptSync();
+// // import promptSync from "prompt-sync";
+// // const prompt = require("prompt-sync")({ sigint: true });
+// // let name = prompt("What is your name?: ");
+// // console.log("Hi ", name);
+
+// // import PromptSync from "prompt-sync";
+// // const prompt = PromptSync();
+// const tempInCelsius = prompt("Enter value in celsius: ");
+// const radius = prompt("radius: ");
+// const height = prompt("height: ");
+// const tempInFahrenheit = (9 / 5) * Number(tempInCelsius) + 32;
+// console.log(tempInFahrenheit);
+let i = 1;
+for (let i = 1; i < 6; i++) {
+  console.log(i);
+  let str = "";
+  for (let j = 1; j < 6; j++) {
+    console.log(str);
+  }
+}
