4a4c2693c81459e6740448ab539535dc635c5e81
Mihret_GetachewE <mgetachew@miu.edu>
2023-10-31 15:11:27 -0500

    add new files

---
+++ w1d2function/function.js
@@ -0,0 +1,20 @@
+// function oddEven(num) {
+//   if (!num) return;
+//   else return true;
+// }
+// console.log(oddEven(7));
+
+function testPrime(num) {
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+    return true;
+  }
+}
+let x = 10;
+function main() {
+  let x;
+  console.log(x);
+}
+main();
